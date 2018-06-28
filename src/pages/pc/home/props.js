fe284c4b5b2ed14a3a1892ab3ccb84e860b572da
LeiZP <leizuoping_lxl@163.com>
2018-06-28 14:36:21 +0800

    pc vue cil

--- src/pages/pc/home/props.js
+++ src/pages/pc/home/props.js
@@ -4,46 +4,46 @@ export default [
     pageOneData: [
       {
         id: 1,
-        title: '来鲨鱼直播，上连线，主播不行你就上',
-        subtitle: '人人看明星，人人当明星',
+        title: '1',
+        subtitle: '2',
         src: ''
       }, {
         id: 2,
-        title: '来鲨鱼直播，看表演，还能斗图',
-        subtitle: '与人斗图，其乐无穷',
+        title: '1',
+        subtitle: '2',
         src: ''
       }, {
         id: 3,
-        title: '来鲨鱼直播，每一票，都决定生死',
-        subtitle: '一人一票，决定直播走向',
+        title: '1',
+        subtitle: '2',
         src: ''
       }, {
         id: 4,
-        title: '来鲨鱼直播，看直播，顺便抽红包',
-        subtitle: '天天看直播，日日抽红包',
+        title: '1',
+        subtitle: '2',
         src: ''
       }
     ],
     pageTwoData: [
       {
         id: 1,
-        title: '鲨鱼直播真▪互动，与粉丝连线，一起尖叫',
-        subtitle: '此刻，高清连线画质直线互联，精彩互动',
+        title: '1',
+        subtitle: '2',
         src: ''
       }, {
         id: 2,
-        title: '鲨鱼直播真▪互动,边斗图，边赚钱',
-        subtitle: '轻松拥有自己的专属表情商店',
+        title: '1',
+        subtitle: '1',
         src: ''
       }, {
         id: 3,
-        title: '鲨鱼直播真▪互动，让粉丝告诉他们的心声',
-        subtitle: '趣味投票欢乐多',
+        title: '1',
+        subtitle: '2',
         src: ''
       }, {
         id: 4,
-        title: '鲨鱼直播真▪互动，直播红包闹翻天',
-        subtitle: '简单返利，让更多人爱上你的直播',
+        title: '1',
+        subtitle: '2',
         src: ''
       }
     ]
